6f2ae1c1a92f71d4711ab298a532c985d779417f
Epshtein Dmitry <letuver@yahoo.com>
2022-01-20 15:38:48 +0200

    form

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -7,17 +7,26 @@ const modal = () => {
     const modalCallback = document.querySelector(".modal-callback");
     const main = document.querySelector(".main-wrapper");
 
+    const form = document.querySelector("form");
+    const formElems = form.querySelectorAll("input");
+
     const handleMenu = e =>{
         e.preventDefault();
         backWind.style.display = "block";
         modWind.style.display = "block";
 
+        formElems.forEach(input => {
+            if(input.type != "submit"){
+                input.value = "";
+            }
+        });
     }; 
 
     const closeMenu = e =>{
         e.preventDefault();
         backWind.style.display = "none";
         modWind.style.display = "none";
+        
     }; 
 
     main.addEventListener("click", e => {
