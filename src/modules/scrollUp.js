19227f6d637cf2e90f2c28d477a8e3486d737882
Epshtein Dmitry <letuver@yahoo.com>
2022-01-22 03:50:21 +0200

    change css

--- src/modules/scrollUp.js
+++ src/modules/scrollUp.js
@@ -2,6 +2,9 @@ const scrollUp = () => {
     const arr = document.querySelector(".up");
 
     arr.addEventListener("click", e => {
+        // let y = document.body.scrollTop;
+        // window.scrollTo(0, 0);
+        // console.log(y);
         let aim = document.querySelector(".main-wrapper");
         aim.scrollIntoView({behavior: "smooth"});
         if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
--- src/modules/slider.js
+++ src/modules/slider.js
@@ -2,22 +2,19 @@ const slider = () => {
     //const sliderBlock = document.querySelector(".top-slider");
     const slides = document.querySelectorAll(".item");
 
-//    console.log(sliderBlock);
-  //  console.log(slides.length);
-
     let currentSlide = 0;
     let interval;
     //let timeInterval = 2000;
 
     const prevSlide = (elements, index, strClass) => {
         elements[index].classList.remove(strClass);
-        elements[index].classList.add("table");
+        //elements[index].classList.add("table");
         elements[index].style.display = "none";
     };
 
     const nextSlide = (elements, index, strClass) => {
         elements[index].classList.add(strClass);
-        elements[index].classList.remove("table");
+        //elements[index].classList.remove("table");
         elements[index].style.display = "block";
     };
 
